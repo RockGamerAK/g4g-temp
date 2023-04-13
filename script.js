95c42f43030f5ffc9909505d3ce4ca9a56d798ad
RockGamerAK <86740712+RockGamerAK@users.noreply.github.com>
2023-04-13 11:32:16 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-let innerHTML = document.documentElement.innerHTML; document.documentElement.innerHTML = 'Allow Pop-Ups to view the site.<script>' + innerHTML.split('</iframe></script>')[1] + '</body></html>'; 
-if (location.href.includes('g4g')) {
-    let newWindow = window.open('about:blank'); 
-    newWindow.document.open(); 
-    newWindow.document.write('<html>' + innerHTML.split('</iframe><script>')[0] + '</iframe></body></html>')
-}; 
-window.close();
+document.querySelector('link[rel="icon"]').href = `https://minim.proxy.rockgamerak.com/user/_${btoa('https://graphics-for-good.rockgamerak.repl.co')}_/favicon.png`
